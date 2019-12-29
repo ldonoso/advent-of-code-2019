bb085e90953ac66c17b5e45d75dbffda792b16d5
ldonoso <ldonosodedios@gmail.com>
2019-12-29 19:11:45 +0000

    Day8 part two

--- Day8.hs
+++ Day8.hs
@@ -5,6 +5,8 @@ import Data.List.Split (splitOn, chunksOf)
 import Data.List
 import Debug.Trace
 import Data.Char
+import Control.Applicative
+import Control.Monad
 
 
 data Image = Image { wide :: Int, tall :: Int }
@@ -20,10 +22,32 @@ resolve image xs = (numberOf 1 layer) * (numberOf 2 layer)
         layers = chunksOf (wide image * tall image) xs
         layer = minimumBy compareLayers layers
         compareLayers a b = compare (numberOf 0 a) (numberOf 0 b)
+
+
+mergePixels :: Int -> Int -> Int
+mergePixels 0 _ = 0
+mergePixels 1 _ = 1
+mergePixels 2 x = x
+
+
+resolve2 :: Image -> [Int] -> [Int]
+resolve2 image xs = getZipList layersFold
+    where
+        layers = fmap ZipList $ chunksOf (wide image * tall image) xs
+        layersFold = foldl1 (\acc x -> mergePixels <$> acc <*> x) layers
+
+
+printImage :: Image -> [Int] -> IO ()
+printImage image xs = forM_ rows (print)
+    where
+          xsChar = fmap (\x -> if x == 1 then '*' else ' ') xs
+          rows = chunksOf (wide image) xsChar
          
 
 main :: IO ()
 main = do
-    contents :: String <- getContents
-    let xs :: [Int] = fmap digitToInt $ takeWhile isDigit contents
-    print $ resolve (Image 25 6) xs
+    contents <- getContents
+    let xs = fmap digitToInt $ takeWhile isDigit contents
+    let image = Image 25 6
+    print $ resolve image xs
+    printImage image $ resolve2 image xs
