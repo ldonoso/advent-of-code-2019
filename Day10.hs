bd56904238ab8f8e13f6aa28b88f50cc37128d67
ldonoso <luisantoniod@gmail.com>
2020-06-20 15:28:14 +0000

    Day 10 code refactoring for part2

--- Day10.hs
+++ Day10.hs
@@ -1,17 +1,22 @@
 module Day10 where
 
 import Control.Monad
-import Control.Arrow((>>>))
-import Data.Function((&))
+import Control.Arrow ((>>>))
+import Data.Function ((&))
 import qualified Data.Set as S
 import qualified Data.List as L
 import Data.Ratio
+import Data.List
+import Debug.Trace
+import Data.Ord (comparing)
+
+myTrace x = trace (show x) x
 
 data Coord = Coord Int Int deriving (Show, Eq, Ord)
 data Polar = Polar {
-                polarAngle :: Float,
-                polarLen :: Float
-             }
+                polarAngle :: Double,
+                polarLen :: Double
+             } deriving (Show)
 
 strToCoord :: String -> [Coord]
 strToCoord s = do
@@ -20,56 +25,35 @@ strToCoord s = do
     guard (c == '#')
     return (Coord x y)
 
-coordToPool :: Coord -> Polar
-coordToPool (Coord x y) =
-    Polar angle len
+coordToPolar :: Coord -> Polar
+coordToPolar (Coord x y) = Polar angle len
     where
         x' = fromIntegral x
         y' = fromIntegral y
-        angle = atan ( y' / x')
+        angle = atan2 y' x'
         len = sqrt (x' ^ 2 + y' ^ 2)
-            
-
-normalize :: Coord -> Coord
-normalize (Coord x y) =
-    if d /= 0
-        then Coord (div x d) (div y d)
-        else Coord x y
-    where d = gcd x y
 
 relocate :: Coord -> Coord -> Coord
-relocate (Coord xC yC) (Coord x y) = Coord (x - xC) (y - yC)
+relocate (Coord xC yC) (Coord x y) = Coord (x - xC) (yC - y)
 
-getNAst :: [Coord] -> Coord -> Int
-getNAst coords center =
-    fmap (relocate center) coords &
-    fmap normalize &
+getNAst2 :: [Coord] -> Coord -> Int
+getNAst2 coords center =
+    fmap (relocate center) (filter ((/=) center) coords) &
+    fmap coordToPolar &
+    fmap polarAngle &
     S.fromList &
-    S.size &
-    (subtract 1)
+    S.size
 
 solve :: String -> String
 solve s = 
     let coords = strToCoord s
-        nAst = fmap (getNAst coords) coords
+        nAst = fmap (getNAst2 coords) coords
         nAstMax = L.maximum nAst
     in show nAstMax
 
-maximum' :: (Ord b) => (a -> b) -> [a] -> a
-maximum' f (x:xs) = fst $ L.foldl' f' (x, f x) xs
-    where f' (y, fy) x =
-            if fx  > fy then (x, fx) else (y, fy)
-            where fx = f x 
-
 getCenter :: [Coord] -> Coord
 getCenter coords =
-    fst $ maximum' snd (zip coords nAst)
-    where nAst = fmap (getNAst coords) coords
-
-solve2 :: String -> String
-solve2 s = 
-    let coords = strToCoord s
-        center = getCenter coords
-    in show center
+    fst $ maximumBy (comparing snd) (zip coords nAst)
+    where nAst = fmap (getNAst2 coords) coords
 
 main = interact solve
